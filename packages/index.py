4da5f46c0d0ff2bcf54e9239dc45b91812a54dc2
python batch <python-batch@gmail.com>
2019-04-24 17:51:20 +0530

    Worked on modules and packages

---
+++ packages/index.py
@@ -0,0 +1,14 @@
+import lib.patterns
+import lib.patterns as pattern
+from lib.patterns import printPattern as pat1, printPattern2 as pat2, printTriangle as pt
+from lib.loops import printLoop, printNumpattern as pnum #Get function from loops modules
+from lib.maths import *
+# lib.patterns.printPattern()
+# pattern.printPattern2()
+# pat1()
+# pat2()
+# printLoop()
+# printMath()
+# printLoop2()
+# pt()
+pnum(4)
