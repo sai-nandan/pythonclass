6913b1858f19d3707b01e2e6c3753034b5a6e07a
kanthi <kanthi@Bhargavs-MacBook-Pro.local>
2019-04-30 12:00:30 +0530

    dont now

--- loops.py
+++ loops.py
@@ -1,22 +1,22 @@
-print("1"*5)
-a=1
-while a < 5:
-    print(a*a)
-    a = a+1
-while a < 10:
-    b = 1
-    while b <= a:
-        print(b,end=" ")
-        b = b + 1
-    a = a+1
-    print()
-while a < 10:
-    b = 1
-    while b <= a:
-        print(a,end=" ")
-        b = b + 1
-    a = a+1
-    print()
+# print("1"*5)
+# a=1
+# while a < 5:
+#     print(a*a)
+#     a = a+1
+# while a < 10:
+#     b = 1
+#     while b <= a:
+#         print(b,end=" ")
+#         b = b + 1
+#     a = a+1
+#     print()
+# while a < 10:
+#     b = 1
+#     while b <= a:
+#         print(a,end=" ")
+#         b = b + 1
+#     a = a+1
+#     print()
 def loops(a):
     while a >= 1:
         b=1
--- sample.py
+++ sample.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Number = int(input("Please Enter any Number: "))
 Sum = 0
 
@@ -8,14 +7,3 @@ while(Number > 0):
     Number = Number //10
 
 print("\n Sum of the digits of Given Number = %d" %Sum)
-=======
-print("Python Batch")
-a = 60;
-
-if True:
-    print("Hello")
-else:
-    print("else")
-
-
->>>>>>> fbb973ef4f6831fe66f1a9b04848623cee561d4f
