dba19c4effb9513f0d525db541fb6ef6a2850546
vinaychowdary302 <chowdary.vinayaluri@gmail.com>
2019-05-08 14:47:38 +0530

    group of dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+main_Dictionaries={
+    'student_ids': {'A101','A102','A103','A104'},
+    'student_info' : {
+        'A101':{'name':'Teja','branch':'CSE','year':4},
+        'A102':{'name':'Vinay','branch':'Mech','year':4},
+        'A103':{'name':'Kanthi','branch':'Civil','year':4},
+        'A104':{'name':'Nandan','branch':'BSC','year':3}
+        },
+    'games' : {'cricket','football','basketball','volleyball','chess'},
+
+    'sports_subscribed' : {
+    'cricket':{'A101','A102','A103'},
+    'basketball':{'A101','A102'},
+    'volleyball':{'A103'}
+        },
+}
